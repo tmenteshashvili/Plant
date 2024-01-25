21b664488c3a824d0153ffe2367d52026cd4b402
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2024-01-25 17:46:04 +0400

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Tako Menteshashvili on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
